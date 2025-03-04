6098294d78827722eacb145a4f65c189ca32aaaa
pedrovotf <pedrovotf@gmail.com>
2025-03-04 17:01:05 -0300

    Download react-scroll and config appBar for lib

--- script/src/app/core/pages/home/page.js
+++ script/src/app/core/pages/home/page.js
@@ -8,6 +8,7 @@ import {
   BoxHomeAboutMe,
   BoxHomeTecnologies
 } from './utils'
+import { Element} from "react-scroll";
 
 const IndexPageHome =()=> {
   const { language } = useLanguageContext();
@@ -37,93 +38,106 @@ const IndexPageHome =()=> {
 
   return (
     <>
-      <ButtonAppBar
-        Typography_variant="h6"
-        Typography_component= "div"
-        BarStyle = {{
-            "Class_appBarDiv":"home-appbar-div",
-            "Class_toolbar":"home-appbar-div-appbar-toolbar",
-            "Class_AppBarTypography":"home-header-appbar-typography",
+ 
+        <ButtonAppBar
+          Typography_variant="h6"
+          Typography_component= "div"
+          BarStyle = {{
+              "Class_appBarDiv":"home-appbar-div",
+              "Class_toolbar":"home-appbar-div-appbar-toolbar",
+              "Class_AppBarTypography":"home-header-appbar-typography",
+              "Class_appBar_breadcrumbs":"home-header-appbar-breadcrumbs",
+              "Class_AppBarTypography_box":"home-header-appbar-typography-box",
+            }}
+          ButtonAppBar = {{
+            "Class_ButtonAppBar":"home-header-button-appbar",
+            "Class_ButtonAppBar_box":"home-header-button-appbar-box",
           }}
-        ButtonAppBar = {{
-          "Class_ButtonAppBar":"home-header-button-appbar",
-          "Class_ButtonAppBar_box":"home-header-button-appbar-box"
-        }}
-        ContentText = {{
-            "optionsAppBar":textPage.optionsAppBar,
-            "themesPage":textPage.themesPage,
-            "languagesPage":textPage.languagesPage
+          ContentText = {{
+              "optionsAppBar":textPage.optionsAppBar,
+              "themesPage":textPage.themesPage,
+              "languagesPage":textPage.languagesPage
+            }}
+          ScrollLinkConfig = {{
+            "ScrollLink_smooth":true,
+            "ScrollLink_duration":500,
+            "ScrollLink_offset":-50,
+            "ScrollLink_spy":true
           }}
-      />
-      <BoxHomeIntro 
-        BoxContentText = {{
-          "introBox":textPage.introBox,
-        }}
-        BoxClasses = {{
-          "Class_Box_Container":"home-intro-box-container",
-          "Class_Box_Grid":"home-intro-box-grid",
-          "Class_Box":"home-intro-box-box",
-          "Class_Box_Typography":"home-intro-box-typography",
-          "Class_Box_Typography_span":"home-intro-box-typography-span",
-          "Class_Box_img":"home-intro-box-img"
-        }}
-        GridConfig = {{
-          "Grid_size":12,
-          "Grid_columns":6
-        }}
-        TypographyConfig = {{
-          "variantH1":"h1",
-          "variantH2":"h2",
-          "typographyIdPrefix":"typography-introBox-prefix-name",
-          "typographyIdName":"typography-introBox-name",
-        }}
-      />
-      <BoxHomeAboutMe 
-        BoxContentText = {{
-          "aboutMe":textPage.aboutMe,
-        }}
-        BoxClasses = {{
-          "Class_Box_Container":"home-about-me-box-container",
-          "Class_Box_Grid":"home-about-me-box-grid",
-          "Class_Box":"home-about-me-box-box",
-          "Class_Box_Typography":"home-about-me-box-typography",
-          "Class_Box_img":"home-about-me-box-img",
-          "Class_Box_Grid_second":"home-about-me-box-grid-second",
-          "Class_Box_Grid_second_box":"home-about-me-box-grid-second-box",
-          "Class_Box_Grid_second_box_button":"home-about-me-box-grid-second-box-button",
-          "Class_Box_Grid_second_box_img":"home-about-me-box-grid-second-box-img",
-        }}
-        GridConfig = {{
-          "Grid_size":12,
-          "Grid_columns":6,
-          "Grind_columns_second":2
-        }}
-        TypographyConfig = {{
-          "variantH1":"h1",
-          "variantBody1":"body1",
-          "typographyIdTitle":"typography-aboutMe-title",
-          "typographyIdBody":"typography-aboutMe-body"
-        }}
-      />
-      <BoxHomeTecnologies 
-        BoxContentText = {{
-          "technologies":textPage.technologies,
-        }}
-        BoxClasses = {{
-          "Class_Box_Container":"home-technologies-box-container",
-          "Class_Box_Grid":"home-technologies-box-grid",
-          "Class_Box_second_grid":"home-technologies-box-grid-second",
-          "Class_Box_Grid_title":"home-technologies-box-grid-title",
-          "Class_Box_img":"home-technologies-box-img",
-        }}
-        TypographyConfig = {{
-          "variant":"h3"
-        }}
-        GridConfig = {{
-          "Grid_size":25,
-          "Grid_columns":5,
-        }}
-      />
+        />
+        <BoxHomeIntro 
+          BoxContentText = {{
+            "introBox":textPage.introBox,
+          }}
+          BoxClasses = {{
+            "Class_Box_Container":"home-intro-box-container",
+            "Class_Box_Grid":"home-intro-box-grid",
+            "Class_Box":"home-intro-box-box",
+            "Class_Box_Typography":"home-intro-box-typography",
+            "Class_Box_Typography_span":"home-intro-box-typography-span",
+            "Class_Box_img":"home-intro-box-img"
+          }}
+          GridConfig = {{
+            "Grid_size":12,
+            "Grid_columns":6
+          }}
+          TypographyConfig = {{
+            "variantH1":"h1",
+            "variantH2":"h2",
+            "typographyIdPrefix":"typography-introBox-prefix-name",
+            "typographyIdName":"typography-introBox-name",
+          }}
+        />
+      <Element key={0} >
+        <BoxHomeAboutMe 
+          BoxContentText = {{
+            "aboutMe":textPage.aboutMe,
+          }}
+          BoxClasses = {{
+            "Class_Box_Container":"home-about-me-box-container",
+            "Class_Box_Grid":"home-about-me-box-grid",
+            "Class_Box":"home-about-me-box-box",
+            "Class_Box_Typography":"home-about-me-box-typography",
+            "Class_Box_img":"home-about-me-box-img",
+            "Class_Box_Grid_second":"home-about-me-box-grid-second",
+            "Class_Box_Grid_second_box":"home-about-me-box-grid-second-box",
+            "Class_Box_Grid_second_box_button":"home-about-me-box-grid-second-box-button",
+            "Class_Box_Grid_second_box_img":"home-about-me-box-grid-second-box-img",
+          }}
+          GridConfig = {{
+            "Grid_size":12,
+            "Grid_columns":6,
+            "Grind_columns_second":2
+          }}
+          TypographyConfig = {{
+            "variantH1":"h1",
+            "variantBody1":"body1",
+            "typographyIdTitle":"typography-aboutMe-title",
+            "typographyIdBody":"typography-aboutMe-body"
+          }}
+        />
+      </Element>
+      <Element key={1} >
+        <BoxHomeTecnologies 
+          BoxContentText = {{
+            "technologies":textPage.technologies,
+          }}
+          BoxClasses = {{
+            "Class_Box_Container":"home-technologies-box-container",
+            "Class_Box_Grid":"home-technologies-box-grid",
+            "Class_Box_second_grid":"home-technologies-box-grid-second",
+            "Class_Box_Grid_title":"home-technologies-box-grid-title",
+            "Class_Box_img":"home-technologies-box-img",
+          }}
+          TypographyConfig = {{
+            "variant":"h3"
+          }}
+          GridConfig = {{
+            "Grid_size":25,
+            "Grid_columns":5,
+          }}
+        />
+      </Element>
     </>
   );
 }
--- script/src/app/module/materialDesing/appbar/appbar.jsx
+++ script/src/app/module/materialDesing/appbar/appbar.jsx
@@ -5,12 +5,18 @@ import {
     AppBar,
     Box,
     Toolbar,
-    Typography
+    Typography,
+    Breadcrumbs,
+    Link as MuiLink
 } from '@mui/material'
+
+import {Link as ScrollLink} from "react-scroll";
 
 import {printf, useLanguageContext} from "../../../global/globalFunctions"
 import appBarContent  from "../../../../../public/content/pages/home.json"
 import MenuDropDown from "../menu/menu"
+
+
 
 const ButtonAppBar = (
     valuesInput,
@@ -27,22 +33,37 @@ const ButtonAppBar = (
                 valuesInput.BarStyle.Class_appBarDiv
             } >
                 <Toolbar className={valuesInput.BarStyle.Class_toolbar}>
-                    {
-                        textPageAppBar.optionsAppBar.map((item, index) => {
-                            return( 
-                            <Typography key={index}
-                                variant={valuesInput.Typography_variant} 
-                                component={valuesInput.Typography_component}
-                                className={
-                                    valuesInput.BarStyle.Class_AppBarTypography
-                                } 
-                                name={item} 
-                                id={item}> 
-                                {item} 
-                            </Typography>)
-                            
-                        })  
-                    }
+                    <Box
+                        className={valuesInput.BarStyle.Class_AppBarTypography_box}
+                        key={null}
+                    >
+                        {
+                            textPageAppBar.optionsAppBar.map((item, index) => {
+                                return( 
+                                    <ScrollLink
+                                        to={index}
+                                        smooth={valuesInput.ScrollLinkConfig.ScrollLink_smooth}
+                                        duration={valuesInput.ScrollLinkConfig.ScrollLink_duration}
+                                        offset={valuesInput.ScrollLinkConfig.ScrollLink_offset}
+                                        spy={valuesInput.ScrollLinkConfig.ScrollLink_spy}
+                                        className='testeee'
+                                    >
+                                        <Typography 
+                                            key={index}
+                                            variant={valuesInput.Typography_variant} 
+                                            component={valuesInput.Typography_component}
+                                            name={item} 
+                                            id={item}
+                                            className={valuesInput.BarStyle.Class_AppBarTypography}
+                                            sx={{ color: (theme) => theme.palette.primary.contrastText }}
+                                        > 
+                                            {item} 
+                                        </Typography>
+                                    </ScrollLink>
+                                )
+                            })  
+                        }
+                    </Box>
                     <MenuDropDown
                         textContent={textPageAppBar.themesPage}
                         typeMenu="theme"
