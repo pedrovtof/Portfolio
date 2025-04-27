0716d2dfd916bf1eaea5706cc4da36586f83c0e6
pedrovotf <pedrovotf@gmail.com>
2025-04-27 19:52:17 -0300

    add footer class

--- script/src/core/layout/footer.jsx
+++ script/src/core/layout/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return(
         <React.Fragment>
             <footer>
-                <p>
+                <p className="footer">
                     ©pedrovotf 
                 </p>
             </footer>
