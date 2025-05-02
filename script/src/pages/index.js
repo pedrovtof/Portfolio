e297940d4181665bb2f2bd91c74bebb5091fbb2c
pedrovotf <pedrovotf@gmail.com>
2025-05-02 12:04:56 -0300

    update title page

--- script/src/pages/index.js
+++ script/src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
 
   return (
      <RootLayout
-      title="Pedro Fonseca - Portfolio"
+      title="Pedro Fonseca"
     >
       <PortifolioPage />
      </RootLayout>
